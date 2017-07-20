70b5edc3025ee7131561f018996096db0da11508
yyamakawa-cq <y.yamakawa@caraquri.com>
2017-07-20 21:01:51 +0900

    errorダイアログのコードを修正

--- app/src/main/java/com/example/ichi/kenshu/ShowErrorDialog.java
+++ app/src/main/java/com/example/ichi/kenshu/ShowErrorDialog.java
@@ -11,16 +11,15 @@ public class ShowErrorDialog {
     public ShowErrorDialog() {}
 
     public void showError(ArrayList error, Context context) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-        alertDialog.setTitle(context.getString(R.string.validation_error));
-        alertDialog.setPositiveButton(context.getString(R.string.button_ok), null);
         StringBuilder messages = new StringBuilder();
-        for (int i = 0; i < error.size(); i++) {
+        for (Object e : error) {
             messages.append("\n");
-            String text = (String) error.get(i);
-            messages.append(text);
+            messages.append(e);
         }
-        alertDialog.setMessage(messages);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.validation_error))
+                .setPositiveButton(context.getString(R.string.button_ok), null)
+                .setMessage(messages);
         alertDialog.show();
     }
 }
