df1229c3d45a0ec56fa0f470f44ae3d8be8393cc
yyamakawa-cq <y.yamakawa@caraquri.com>
2017-07-20 11:36:11 +0900

    エラーダイアログ表示のコードの文字列をstring.xmlを参照するように修正

--- app/src/main/java/com/example/ichi/kenshu/ShowErrorDialog.java
+++ app/src/main/java/com/example/ichi/kenshu/ShowErrorDialog.java
@@ -1,6 +1,6 @@
 package com.example.ichi.kenshu;
 
-import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AlertDialog;
 
 import java.util.ArrayList;
@@ -10,10 +10,10 @@ public class ShowErrorDialog {
 
     public ShowErrorDialog() {}
 
-    public void showError(ArrayList error, Activity activity) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(activity);
-        alertDialog.setTitle("エラー");
-        alertDialog.setPositiveButton("OK", null);
+    public void showError(ArrayList error, Context context) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+        alertDialog.setTitle(context.getString(R.string.validation_error));
+        alertDialog.setPositiveButton(context.getString(R.string.button_ok), null);
         StringBuilder messages = new StringBuilder();
         for (int i = 0; i < error.size(); i++) {
             messages.append("\n");
