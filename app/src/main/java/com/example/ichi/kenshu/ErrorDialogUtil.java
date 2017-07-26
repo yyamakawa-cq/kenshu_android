b7ef035d6d9e32ada5297c51d585b1088d93ccbf
yyamakawa-cq <y.yamakawa@caraquri.com>
2017-07-26 18:47:37 +0900

    配列の文字列結合をTextUtils.joinを使った方法で書き換え

--- app/src/main/java/com/example/ichi/kenshu/ErrorDialogUtil.java
+++ app/src/main/java/com/example/ichi/kenshu/ErrorDialogUtil.java
@@ -2,6 +2,7 @@ package com.example.ichi.kenshu;
 
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 
 import java.util.List;
 
@@ -9,10 +10,7 @@ import java.util.List;
 public class ErrorDialogUtil {
 
     public static void showError(List<String> error, Context context) {
-        StringBuilder messages = new StringBuilder();
-        for (String e : error) {
-            messages.append("\n").append(e);
-        }
+        String messages = TextUtils.join("\n",error);
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(context)
                 .setTitle(context.getString(R.string.validation_error))
                 .setPositiveButton(context.getString(R.string.button_ok), null)
