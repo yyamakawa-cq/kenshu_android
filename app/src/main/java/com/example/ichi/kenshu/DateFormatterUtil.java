1b83362cee6261c397850818f3cf8483c840516e
yyamakawa-cq <y.yamakawa@caraquri.com>
2017-08-09 14:43:02 +0900

    DateFormatの文字列を定数に修正&エラー時のログ出力を修正

--- app/src/main/java/com/example/ichi/kenshu/DateFormatterUtil.java
+++ app/src/main/java/com/example/ichi/kenshu/DateFormatterUtil.java
@@ -1,21 +1,25 @@
 package com.example.ichi.kenshu;
 
 
+import android.util.Log;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class DateFormatterUtil {
+    private static final String API_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
+    private static final String APP_DATE_FORMAT = "yyyy/MM/dd";
 
     public static String changeDateFormat(String purchaseDate) {
         Date date;
         String changedDate = null;
         try {
-            date = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US).parse(purchaseDate);
-            changedDate = new SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN).format(date);
+            date = new SimpleDateFormat(API_DATE_FORMAT, Locale.US).parse(purchaseDate);
+            changedDate = new SimpleDateFormat(APP_DATE_FORMAT, Locale.JAPAN).format(date);
         } catch (ParseException e) {
-            e.printStackTrace();
+            Log.d("error", "ParseException");
         }
         return changedDate;
     }
